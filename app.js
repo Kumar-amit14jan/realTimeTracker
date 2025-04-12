1f4fe7310ff3d60f07f1540f77a8a9fff849a0fb
Amit Kumar <51708840+Kumar-amit14jan@users.noreply.github.com>
2025-04-12 11:37:29 +0000

    remove hard cording of the port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const http = require('http');
 const path = require('path');
 const sockerio = require('socket.io');
